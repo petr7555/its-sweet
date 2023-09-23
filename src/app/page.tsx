54f71f22135b00448cc76d05181a1cf1816b67c4
Petr Janik <peta.janik@email.cz>
2023-09-23 14:20:11 +0200

    Change favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ import Image from "next/image";
 
 const Home = () => (
     <>
-    <main className="flex flex-col items-center justify-center p-24">
-        <p className="text-5xl lg:text-6xl font-semibold text-center">It&apos;s pronounced &quot;sweet&quot;</p>
-        <div
-            className="flex flex-col lg:flex-row items-center justify-center gap-8 mt-10">
-            <PronunciationColumn word="suite" pronunciation="/swiːt/" audioSrc={SUITE_AUDIO_SRC}/>
-            <PronunciationColumn word="sweet" pronunciation="/swiːt/" audioSrc={SWEET_AUDIO_SRC}/>
-        </div>
-    </main>
+        <main className="flex flex-col items-center justify-center p-24">
+            <p className="text-5xl lg:text-6xl font-semibold text-center">It&apos;s pronounced &quot;sweet&quot;</p>
+            <div
+                className="flex flex-col lg:flex-row items-center justify-center gap-8 mt-10">
+                <PronunciationColumn word="suite" pronunciation="/swiːt/" audioSrc={SUITE_AUDIO_SRC}/>
+                <PronunciationColumn word="sweet" pronunciation="/swiːt/" audioSrc={SWEET_AUDIO_SRC}/>
+            </div>
+        </main>
         <footer>
             <div className="flex flex-col items-center justify-center p-4">
                 <p className="text-xl font-semibold text-center">About the project:</p>
@@ -30,6 +30,12 @@ const Home = () => (
                     />
                     <p className="text-xl font-semibold">GitHub</p>
                 </a>
+                <p className="text-sm font-semibold text-center mt-4">
+
+                    Favicon by <a href="https://www.flaticon.com/free-icons/ice-cream" title="ice-cream favicon"
+                                  className="text-blue-500">Freepik
+                    - Flaticon</a>
+                </p>
             </div>
         </footer>
     </>
