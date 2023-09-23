5ba1d5b62edbc997c38f3e866b8f5541167da00f
Petr Janik <peta.janik@email.cz>
2023-09-23 14:02:04 +0200

    Add "About the project" footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,38 @@
-import { SUITE_AUDIO_SRC, SWEET_AUDIO_SRC } from '@/app/constants';
+import {SUITE_AUDIO_SRC, SWEET_AUDIO_SRC} from '@/app/constants';
 import PronunciationColumn from '@/app/PronunciationColumn';
+import Image from "next/image";
 
 
 const Home = () => (
-  <main className="flex flex-col items-center justify-center p-24">
-    <p className="text-5xl lg:text-6xl font-semibold text-center">It&apos;s pronounced &quot;sweet&quot;</p>
-    <div
-      className="flex flex-col lg:flex-row items-center justify-center gap-8 mt-10">
-      <PronunciationColumn word="suite" pronunciation="/swiːt/" audioSrc={SUITE_AUDIO_SRC}/>
-      <PronunciationColumn word="sweet" pronunciation="/swiːt/" audioSrc={SWEET_AUDIO_SRC}/>
-    </div>
-  </main>
+    <>
+    <main className="flex flex-col items-center justify-center p-24">
+        <p className="text-5xl lg:text-6xl font-semibold text-center">It&apos;s pronounced &quot;sweet&quot;</p>
+        <div
+            className="flex flex-col lg:flex-row items-center justify-center gap-8 mt-10">
+            <PronunciationColumn word="suite" pronunciation="/swiːt/" audioSrc={SUITE_AUDIO_SRC}/>
+            <PronunciationColumn word="sweet" pronunciation="/swiːt/" audioSrc={SWEET_AUDIO_SRC}/>
+        </div>
+    </main>
+        <footer>
+            <div className="flex flex-col items-center justify-center p-4">
+                <p className="text-xl font-semibold text-center">About the project:</p>
+                <a
+                    className="flex items-center justify-center gap-2 mt-2"
+                    href="https://github.com/petr7555/its-sweet#about-the-project"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <Image
+                        src="/github_mark.svg"
+                        alt="GitHub icon"
+                        width={30}
+                        height={30}
+                        priority
+                    />
+                    <p className="text-xl font-semibold">GitHub</p>
+                </a>
+            </div>
+        </footer>
+    </>
 );
 
 export default Home;
